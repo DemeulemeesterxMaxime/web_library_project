93d489a48182328e03bd56ed55dc5b2058e19ee6
JulienDeb <julien.debril@student.junia.com>
2026-03-02 19:30:00 +0100

    feat(front): wire CatalogPage as homepage + update styles

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,27 +1,8 @@
 import './App.css'
+import { CatalogPage } from './vinyls/components/CatalogPage'
 
 function App(): React.JSX.Element {
-  return (
-    <section className="home-page">
-      <h1>Sillon</h1>
-      <p>
-        Bienvenue dans l&apos;underground du vinyle. Explore les albums,
-        découvre les artistes et prépare les pages clients/ventes des prochaines
-        phases.
-      </p>
-      <div className="home-links">
-        <a className="home-link" href="/vinyls">
-          Voir les vinyles
-        </a>
-        <a className="home-link" href="/artists">
-          Voir les artistes
-        </a>
-        <a className="home-link" href="/clients">
-          Voir les clients
-        </a>
-      </div>
-    </section>
-  )
+  return <CatalogPage />
 }
 
 export default App
