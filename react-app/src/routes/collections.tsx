eb44552855e3c047aade3a966d39c1c76623c718
JulienDeb <julien.debril@student.junia.com>
2026-03-03 17:15:00 +0100

    fix(routes): add explicit return types to route components

--- react-app/src/routes/collections.tsx
+++ react-app/src/routes/collections.tsx
@@ -1,5 +1,5 @@
 import { createFileRoute, Outlet } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/collections')({
-  component: () => <Outlet />,
+  component: (): React.JSX.Element => <Outlet />,
 })
