6215eea4e472702fa47fd54ba4fbaab977a5f50b
Maxence-Gibaru <maxence.gibaru@student.junia.com>
2026-03-04 16:00:00 +0100

    fix(antd): add patch for React 19 compatibility

--- react-app/src/main.tsx
+++ react-app/src/main.tsx
@@ -1,3 +1,4 @@
+import '@ant-design/v5-patch-for-react-19'
 import { StrictMode } from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider, createRouter } from '@tanstack/react-router'
